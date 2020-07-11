3990b778d9758ced96e23f5fe7f523cae409359d
zxn-rcy <913883783@qq.com>
2020-07-11 10:35:58 +0800

    jenkins首次提交java项目

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
